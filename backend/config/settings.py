e70803d266eee06f028f0c2339de2821ccecd685
filipraus <filipraus@gmail.com>
2022-01-05 19:45:38 +0100

    preparing the deployment of the 0.1.0 version

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -156,6 +156,10 @@ STATIC_URL = '/static/'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, '../../frontend')
 
+STATICFILES_DIRS = (
+    os.path.join(REACT_APP_DIR, 'build', 'static'),   
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
