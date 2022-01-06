73d272f944cdc9a62f2407b894d1a624eb8a2299
filipraus <filipraus@gmail.com>
2022-01-06 17:33:08 +0100

    preparing the deployment of the 0.1.0 version

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('', include('djangoapp.urls')),
 ]
