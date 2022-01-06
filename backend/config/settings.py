06b5cb352c7247e2ae7f59f2ee5f73279570f63b
filipraus <filipraus@gmail.com>
2022-01-06 08:07:05 +0100

    preparing the deployment of the 0.1.0 version

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-REACT_APP_DIR = '/app/frontend/public/index.html'
+REACT_APP_DIR = '/app/frontend/'
 STATICFILES_DIRS = (
     os.path.join(REACT_APP_DIR, 'public', 'static'),   
 )
