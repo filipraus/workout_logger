fc584bde221f4424c45692e49bc311754d31cf71
filipraus <filipraus@gmail.com>
2022-01-06 23:01:16 +0100

    preparing the deployment of the 0.1.0 version

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,6 +70,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = []
 
