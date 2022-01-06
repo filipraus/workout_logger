f54925c8569595cd42a1e2eaeea9fcbb09c8199b
filipraus <filipraus@gmail.com>
2022-01-06 07:33:43 +0100

    preparing the deployment of the 0.1.0 version

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-
-from '../.djangoapp/views.py' import FrontendAppView;
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('djangoapp.urls')),
-    re_path(r'^', views.FrontendAppView.as_view()),
 ]
